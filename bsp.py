8809aff64017fa019e029db588469021c95a5345
chlohal <me@clh.sh>
2023-09-29 11:08:19 -0400

    Add SVG generation code for debug purposes

--- bsp.py
+++ bsp.py
@@ -25,19 +25,23 @@ def point_in_front_of_line(line: Line, point: Point) -> Ordering:
         else:
             return "Equal"
 
+
 def get_slope(line: Line) -> float:
     (x1, y1), (x2, y2) = line
     return (y1 - y2) / (x1 - x2)
 
+
 def is_vertical(line: Line) -> bool:
     (x1, _), (x2, _) = line
-    
+
     return x1 == x2
+
 
 def are_parallel(line_a, line_b):
     if is_vertical(line_a) and is_vertical(line_b):
         return True
-    
+
+
 def x_coord_on_line(line: Line, x: float):
     # We only need one point & the slope
     _, (x2, y2) = line
@@ -47,6 +51,8 @@ def x_coord_on_line(line: Line, x: float):
 
 # Taken from https://stackoverflow.com/questions/20677795/how-do-i-compute-the-intersection-point-of-two-lines
 # Modified to return None instead of raising
+
+
 def find_intersection(line1: Line, line2: Line) -> Union[Point, None]:
     xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
     ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
@@ -56,14 +62,12 @@ def find_intersection(line1: Line, line2: Line) -> Union[Point, None]:
 
     div = det(xdiff, ydiff)
     if div == 0:
-       return None
+        return None
 
     d = (det(*line1), det(*line2))
     x = det(d, xdiff) / div
     y = det(d, ydiff) / div
     return x, y
-
-            
 
 
 def split_line_segment_with_line(line: Line, segment: Line) -> list[tuple[Line, Ordering]]:
@@ -75,16 +79,19 @@ def split_line_segment_with_line(line: Line, segment: Line) -> list[tuple[Line, 
         return [(segment, point_a_position)]
     else:
         intersection = find_intersection(line, segment)
-        if intersection == None: raise ValueError("Couldn't find intersection")
+        if intersection == None:
+            raise ValueError("Couldn't find intersection")
 
         return [((point_a, intersection), point_a_position),
                 ((intersection, point_b), point_b_position)
                 ]
 
+
 def is_single_point(line: Line) -> bool:
     (x1, y1), (x2, y2) = line
 
     return x1 == x2 and y1 == y2
+
 
 def find_axial_line_index(lines: list[Line]):
     for i, line in enumerate(lines):
@@ -93,6 +100,20 @@ def find_axial_line_index(lines: list[Line]):
         if line[0][1] == line[1][1]:
             return i
     return 0
+
+
+def svg_of_line(line: Line, stroke_color = "black") -> str:
+    (x1, y1), (x2, y2) = line
+
+    return f'<path d="M {x1} {y1} L {x2} {y2}" style="stroke: {stroke_color}; stroke-width: 0.1"/>'
+
+
+def svg_of_line_system(lines: list[Line], view_box: tuple[float, float, float, float] = (0, 0, 10, 10)) -> str:
+    view_box_svg_attr = " ".join(str(n) for n in view_box)
+
+    return (f'<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="{view_box_svg_attr}">'
+            + "".join(svg_of_line(line) for line in lines)
+            + "</svg>")
 
 
 class BinarySpaceTree:
@@ -105,12 +126,19 @@ class BinarySpaceTree:
         self.right = r
         self.value = v
 
+    def in_order_lines(self):
+        left_lines = []
+        right_lines = []
+        if self.left != None:
+            left_lines = self.left.in_order_lines()
+        if self.right != None:
+            right_lines = self.right.in_order_lines()
+
+        return left_lines + self.value + right_lines
+
     def print_in_order_repr(self):
-        if self.left != None: self.left.print_in_order_repr()
-
-        print(self.value)
-
-        if self.right != None: self.right.print_in_order_repr()
+        for line in self.in_order_lines():
+            print(line)
 
 
 def bsp(image_lines: list[Line], start_partition_at_index: Union[int, None] = None) -> Union[BinarySpaceTree, None]:
@@ -124,7 +152,7 @@ def bsp(image_lines: list[Line], start_partition_at_index: Union[int, None] = No
 
     partition_line = image_lines[start_partition_at_index]
 
-    left, right, on_partition = [], [], []
+    in_front, in_behind, on_partition = [], [], []
 
     for segment in image_lines:
         # Ignore self
@@ -136,30 +164,36 @@ def bsp(image_lines: list[Line], start_partition_at_index: Union[int, None] = No
             partition_line, segment)
         for sub_segment, ordering in split_and_ordered:
             # Ignore lines like (x, y), (x, y) which only cover a single point
-            if is_single_point(sub_segment): continue
+            if is_single_point(sub_segment):
+                continue
 
             match ordering:
-                case "Less": left.append(sub_segment)
-                case "More": right.append(sub_segment)
+                case "Less": in_front.append(sub_segment)
+                case "More": in_behind.append(sub_segment)
                 case "Equal": on_partition.append(sub_segment)
 
-    left_node = bsp(left)
-    right_node = bsp(right)
+    right_node = bsp(in_front)
+    left_node = bsp(in_behind)
 
     return BinarySpaceTree(left_node, right_node, on_partition)
 
 
 if __name__ == "__main__":
-    image = [ ((0, 0), (10, 0)), # - top
-             ((9, 0), (9, 9)), # | right 
-             ((1, 0), (0, 9)), # | left 
-             ((1, 8), (9, 8)), # - bottom
+    import sys
 
-             ((2, 2), (3, 3)), # \ 
-             ((2, 2), (2, 3)), # |
-             ((2, 3), (3, 3)), # -
-            ]
+    image = [((0, 0), (10, 0)),  # - top
+             ((1, 0), (1, 9)),  # | left
+             ((9, 0), (9, 9)),  # | right
+             ((1, 8), (9, 8)),  # - bottom
+
+             ((2, 2), (3, 3)),  # \
+             ((2, 2), (2, 3)),  # |
+             ((2, 3), (3, 3)),  # -
+             ]
+
+    space_tree = bsp(image, 0)
     
-    print("done!")
-
-    bsp(image, 0).print_in_order_repr()
+    if "svg" in sys.argv:
+        print(svg_of_line_system(space_tree.in_order_lines()))
+    else:
+        space_tree.print_in_order_repr()    
